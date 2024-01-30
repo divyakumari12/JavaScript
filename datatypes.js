d3016314333694449189a4791745ee485fd5951b
Divya7364 <divya7364sharma@gmail.com>
2024-01-31 02:13:42 +0530

    new

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)  // We are using nodeJs, not Browser
+
+console.log(3
+    +3) // Code readability should be high
+
+    console.log("Divya")
+
+    let name = "Divya" // String
+    let age = 22  //number
+    let isLoggedIn = false //Boolean
+    let state;
+
+    //number => 2 to power 53(range)
+    //bigint 
+    //string => ""
+    //boolean => true/false
+    //null (represenation of empty value)=> standalone value
+    //undefined =>
+    //Symbol => unique
+
+    // object
+    
+    console.log(typeof "Divya")
+    console.log(typeof age)
+    console.log(typeof null)  //object
+    console.log(typeof undefined) //undefined
